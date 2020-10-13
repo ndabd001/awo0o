e45ef501fe4e2cd56109f7c3da6d042e584b7bc4
ndabd001 <ndabd001@fiu.edu>
2020-10-13 01:54:12 -0400

    added a dropdown button and ui framework

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,19 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import BackgroundImage from "../components/image"
 import SEO from "../components/seo"
+import { ThemeProvider, createTheme, Arwes, Button, Frame } from 'arwes'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Awo0o" />
-    
+
+    <ThemeProvider theme={createTheme()}>
+      <Arwes background='noBackground'>
+        <Button animate>Select...</Button>
+      </Arwes>
+    </ThemeProvider>
+
   </Layout>
-)
+);
 
 export default IndexPage
