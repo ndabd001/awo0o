f6e82c0dbc0368b3ddf737f69b95fc9ca652d1d8
ndabd001 <ndabd001@fiu.edu>
2020-11-07 01:32:50 -0500

    fixed empty file

---
+++ src/pages/magic.js
@@ -0,0 +1,12 @@
+//this page will display the pokedex oriented to character's magic descriptions
+import React from 'react'
+
+const magic = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default magic
